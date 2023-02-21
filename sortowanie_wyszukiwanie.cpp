ed72deeb2b2466ad4969c55a4353229b4b3d1f97
Bartosz456 <bartoszsz14@gmail.com>
2023-02-21 19:57:29 +0100

    sortowanie przez wybieranie

---
+++ sortowanie_wyszukiwanie.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int const N = 20;
+int tab[N];
+
+void wypelnij()
+{
+    srand(time(NULL));
+	for (int i = 0; i < N; i++)
+	{
+		tab[i] = rand() % 50;
+	}
+}
+
+void wyswietl()
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << tab[i] << " ";
+	}
+}
+
+void sortowanie()
+{
+	int j, k;
+	for (int i = 0; i < N; i++)
+	{
+		j = i;
+		while (j > 0 && tab[j-1] > tab[j])
+		{
+			k = tab[j];
+			tab[j] = tab[j - 1];
+			tab[j - 1] = k;
+			j--;
+		}
+	}
+}
+int main()
+{
+	cout << "Tablica przed: \n";
+	wypelnij();
+	wyswietl();
+	cout << "\n";
+	cout << "Tablica po sortowaniu przez wybieranie: \n";
+	sortowanie();
+	wyswietl();
+}
